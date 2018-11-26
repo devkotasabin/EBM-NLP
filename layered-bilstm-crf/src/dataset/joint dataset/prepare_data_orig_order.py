4a2d8121db8874884d0b3ff83327e52244b2dad9
Sabin Devkota <devkotasabin@gmail.com>
2018-11-25 21:05:23 -0700

    Added Nested NER Implementation

---
+++ layered-bilstm-crf/src/dataset/joint dataset/prepare_data_orig_order.py
@@ -0,0 +1,48 @@
+import sys
+
+FILENAME = sys.argv[1]
+print FILENAME
+OUTDIR = "joint_BIO_orig_order"
+
+# Go through the columns
+# Prepare the data file
+# Skip 2nd column i.e. the POS data
+
+# Replace the normal tag with B- or I- tag
+# Add a column with 'O' tag for nested LSTM implementation
+
+prev_lbl1 = 'O'
+prev_lbl2 = 'O'
+
+label1_cpy = 'O'
+label2_cpy = 'O'
+
+with open(FILENAME) as f:
+	with open(OUTDIR + '/' + FILENAME, 'w') as write_file:
+		for line in f:
+			if line.strip() != '':
+				line = line.rstrip()
+				token, pos, label1, label2 = line.split('\t')
+				label1_cpy = label1
+				label2_cpy = label2
+				if label1 != 'O':
+					if prev_lbl1 == 'O':
+						label1 = 'B-' + label1
+					else:
+						label1 = 'I-' + label1
+
+				if label2 != 'O':
+					if prev_lbl2 == 'O':
+						label2 = 'B-' + label2
+					else:
+						label2 = 'I-' + label2
+
+
+				write_file.write('%s\t%s\t%s\tO\n' %(token, label2, label1))
+			else:
+				write_file.write(line)
+				label1_cpy = 'O' 
+				label2_cpy = 'O'
+
+			prev_lbl1 = label1_cpy
+			prev_lbl2 = label2_cpy
